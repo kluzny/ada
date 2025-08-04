c23bc2cbbcfff7ac0783aa1c728f7e3b4e410e65
Kyle Luzny <kluzny@gmail.com>
2025-08-04 02:43:02 -0500

    switch entry to a pydantic model

--- ada/entry.py
+++ ada/entry.py
@@ -1,24 +1,15 @@
-class Entry:
+from pydantic import BaseModel
+
+
+class Entry(BaseModel):
     """
     A structured entry in a Conversation
     """
 
     author: str
     body: str
-    role: str
-    content: dict | None
-
-    def __init__(
-        self,
-        author: str,
-        body: str,
-        role: str = "user",
-        content: dict | None = None,
-    ):
-        self.author = author
-        self.body = body
-        self.role = role
-        self.content = content
+    role: str = "user"
+    content: str | None = None
 
     def message(self) -> dict:
         if self.content is None:
@@ -33,6 +24,3 @@ class Entry:
 
     def __str__(self):
         return f"{self.author}: {self.body}"
-
-    def __repr__(self):
-        return f"Entry(author={self.author}, body={self.body}), role={self.role}, content={self.content})"
